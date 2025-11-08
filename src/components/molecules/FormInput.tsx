a8b88cc083007981161c368517ac8c2380db072e
monomonosu <pojinyan@yahoo.co.jp>
2025-11-08 23:21:56 +0900

    必須タグ表示ロジック追加

--- src/components/molecules/FormInput.tsx
+++ src/components/molecules/FormInput.tsx
@@ -1,17 +1,23 @@
 type Props = {
   label: string;
+  required?: boolean;
   children: React.ReactNode;
 };
 
 export const FormInput: React.FC<Props> = (props: Props) => {
-  const { label, children } = props;
+  const { label, required, children } = props;
 
   return (
     <>
-      <div>
+      <div className="flex mb-1">
         <label htmlFor="input" className="text-sm">
           {label}
         </label>
+        {required && (
+          <span className="ml-2 px-1 py-0.5 text-xs text-white bg-red-500 rounded-sm">
+            必須
+          </span>
+        )}
       </div>
       {children}
     </>
