da4da5025953de3be15ff27cf50f45f25baa5e8d
monomonosu <pojinyan@yahoo.co.jp>
2025-11-19 03:18:12 +0900

    ご購入手続きページのバリデーションスキーマ作成

---
+++ src/app/schema.ts
@@ -0,0 +1,33 @@
+import z from "zod";
+import {
+  maxNumberLengthSchema,
+  requiredString,
+} from "@/utils/validation/universalSchema";
+
+// NOTE:schemaファイルはそれぞれページ毎ののfeatureディレクトリで作成するようなイメージです。今回はページが１ページになるのでtopに置いています。
+
+export const purchaseSchema = z.object({
+  last_name: requiredString("姓"),
+  first_name: requiredString("名"),
+  phone_number: maxNumberLengthSchema("電話番号", 11),
+  post_number: maxNumberLengthSchema("郵便番号", 7),
+  prefecture: requiredString("都道府県"),
+  city: requiredString("市区町村"),
+  address: requiredString("番地"),
+  building: z.string().optional(),
+  payment_type: requiredString("支払い方法"),
+});
+
+export type PurchaseForm = z.infer<typeof purchaseSchema>;
+
+export const defaultValues: PurchaseForm = {
+  last_name: "",
+  first_name: "",
+  phone_number: "",
+  post_number: "",
+  prefecture: "",
+  city: "",
+  address: "",
+  building: "",
+  payment_type: "",
+};
