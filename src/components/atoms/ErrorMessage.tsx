984556a549422fca85393175420d527164fa588f
monomonosu <pojinyan@yahoo.co.jp>
2025-11-19 03:15:33 +0900

    エラーメッセージコンポーネント作成

---
+++ src/components/atoms/ErrorMessage.tsx
@@ -0,0 +1,24 @@
+import { HTMLAttributes } from "react";
+
+interface ErrorMessageProps extends HTMLAttributes<HTMLDivElement> {
+  message: string | undefined;
+}
+
+export const ErrorMessage = ({
+  message,
+  className,
+  ...props
+}: ErrorMessageProps) => {
+  return (
+    <>
+      {message && (
+        <div
+          className={`text-sm text-red-500 mt-1 ${className ?? ""}`}
+          {...props}
+        >
+          {message}
+        </div>
+      )}
+    </>
+  );
+};
